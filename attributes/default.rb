0129f302febdabe0b3e75c76f3d2d5ec46e4b099
git <dsklopp@gmail.com>
2015-06-11 18:03:19 -0700

    Adding quotes to ip address

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['rundeck']['admin']['encrypted_data_bag']=false
-default['rundeck']['node_name']=192.168.17.19
-default['rundeck']['hostname']=192.168.17.19
+default['rundeck']['node_name']='192.168.17.19'
+default['rundeck']['hostname']='192.168.17.19'
 default['rundeck']['url']="http://#{node['rundeck']['hostname']}:#{node['rundeck']['port']}"
 default['rundeck']['enable_api_token'] = true
